3f1441c5d2dd148527559ad0c55f4c9fb8a013e0
Cyprian2214 <cyprian2214@gmail.com>
2026-02-26 22:31:55 +0200

    Point login API to Render backend

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:4000";
+const API_BASE = "https://googlekeepclone-api.onrender.com";
 const AUTH_TOKEN_KEY = "authToken";
 
 const existingToken = localStorage.getItem(AUTH_TOKEN_KEY);
